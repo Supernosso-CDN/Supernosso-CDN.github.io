5e76ab1fe8759c4f791b25f1064b51e5bb420b32
luizmaciel-supernosso <luiz.maciel@supernosso.com.br>
2021-04-08 16:12:04 -0300

    erro de cep

--- arquivos/supernosso-theme.js
+++ arquivos/supernosso-theme.js
@@ -56044,16 +56044,16 @@
           });
   
           if (userResponse.IsUserDefined) {
-            if (!of.shippingData.address.postalCode) {
-              $.when(that.getLoggedUserPostalCode(userResponse.Email)).done(function (res) {
-                if (res.length > 0) {
-                  var postalCodeInputFormatted = res[0].postalCode.replace('-', '');
-                  that.setDeliveryShippingData(postalCodeInputFormatted).done(function () {
-                    //that.removeStorage('selectedPickup');
-                  });
-                }
-              });
-            }
+            //if (!of.shippingData.address.postalCode){
+            $.when(that.getLoggedUserPostalCode(userResponse.Email)).done(function (res) {
+              if (res.length > 0) {
+                var postalCodeInputFormatted = res[0].postalCode.replace('-', '');
+                that.setDeliveryShippingData(postalCodeInputFormatted).done(function () {
+                  //that.removeStorage('selectedPickup');
+                });
+              }
+            });
+            //}
           }
   
           $('.enter-postal-code').on('click', async function () {
